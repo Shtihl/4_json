958219cd1fcd96fcb31e44368b23c3317e260a73
Shtihl <shtihlfd@yandex.ru>
2017-11-20 04:51:19 +0300

    Some refactor of pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -3,24 +3,17 @@ import sys
 
 
 def load_json_data(filepath):
-    with open(filepath, 'r', encoding='utf-8') as source_file:
-        json_content = json.load(source_file)
-    return json_content
+    with open(filepath, 'r', encoding='utf-8') as json_file:
+        return json.load(json_file)
 
 
 def pretty_print_json(data):
-    pretty_json = json.dumps(data,
-                             sort_keys=True,
-                             indent=4,
-                             ensure_ascii=False)
-    print(pretty_json)
+    print(json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        source_file_path = sys.argv[1]
-        loaded_json = load_json_data(source_file_path)
-        pretty_print_json(loaded_json)
+        filepath = sys.argv[1]
+        pretty_print_json(load_json_data(filepath))
     else:
         print("Usage: python pprint_json.py path_to_json_file")
-
