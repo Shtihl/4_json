05c98e1743291f4e6f2758a1a18b9f409d2f5056
Shtihl <shtihlfd@yandex.ru>
2017-11-20 05:12:51 +0300

    Some rename in pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -7,8 +7,8 @@ def load_json_data(filepath):
         return json.load(json_file)
 
 
-def pretty_print_json(data):
-    print(json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False))
+def pretty_print_json(json_data):
+    print(json.dumps(json_data, sort_keys=True, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
